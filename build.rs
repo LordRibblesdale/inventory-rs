dce3a5b423e0e2eab53f473cd82f8a6214bc99d1
Domenico Verde <domenico.verde@aol.com>
2023-04-30 20:59:48 +0200

    Add protobuf message creation at compile-time

--- build.rs
+++ build.rs
@@ -1,3 +1,11 @@
-fn main() {
+use std::io::Result;
 
+fn main() -> Result<()> {
+    // println!("cargo:rerun-if-changed=protobuf/database.proto");
+
+    prost_build::Config::new()
+        .out_dir("src/database")
+        .default_package_filename("database")
+        .compile_protos(&["database.proto"], &["protobuf"])?;
+    Ok(())
 }
